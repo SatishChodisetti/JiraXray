20f3647d8a853dcc712be292967e031e7a0e3e71
Satish Chodisetti <satishchodisetti@Satishs-MacBook-Pro.local>
2024-11-17 14:09:10 +0530

    First Commit for Jira Xray Integration

---
+++ src/test/java/seleniumPractice/StringEx.java
@@ -0,0 +1,33 @@
+package seleniumPractice;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StringEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter");
+		String s=sc.nextLine();
+		System.out.println("Enter Username:");
+		System.out.println(s);
+		
+		//
+		char[]ch=s.toCharArray();
+		
+		
+		ArrayList<Character> list= new ArrayList<Character>();
+		
+		
+		
+		
+		
+	 
+	
+
+	}
+
+}
